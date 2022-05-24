4638c6cf50b6989d0e5e45317f97f6df13523ca8
Anthony <anthonyly@hotmail.ca>
2022-05-24 15:59:06 -0400

    create game logic

---
+++ scripts.js
@@ -0,0 +1,108 @@
+const SELECTION = ["rock", "paper", "scissors"];
+const HAND_WINS = {
+	rock: { paper: false, scissors: true },
+	paper: { scissors: false, rock: true },
+	scissors: { rock: false, paper: true },
+};
+
+let playerScore = 0;
+let computerScore = 0;
+
+// select random hand choice 
+let computerPlay = () =>
+	SELECTION[Math.floor(Math.random() * SELECTION.length)];
+
+
+// returns true if player wins
+function playRound(playerSelection, computerSelection) {
+	// tie
+	if (playerSelection === computerSelection) {
+		return false;
+	}
+
+	return playerSelection[computerSelection];
+
+}
+
+/**
+ *
+ * @param {boolean} win
+ * @param {string} playerSelection
+ * @param {string} computerSelection
+ */
+function printOutcome(win, playerSelection, computerSelection) {
+	if (win) {
+		console.log(
+			"Player wins with %s, beating Computer with %s",
+			playerSelection,
+			computerSelection
+		);
+	} else {
+		console.log(
+			"Player loses with %s, lost to Computer with %s",
+			playerSelection,
+			computerSelection
+		);
+	}
+}
+
+// plays the game for 5 rounds and keeps track of the score
+function game() {
+	let computerScore = 0;
+	let playerScore = 0;
+
+	// prompt user for input
+
+	for (let i = 0; i < 5; i++) {
+		let playerSelection = getPlayerSelection();
+		let computerSelection = computerPlay();
+
+		console.log(computerSelection);
+
+		playRound(playerSelection, computerSelection)
+
+
+
+
+	}
+}
+
+function getPlayerSelection() {
+	while (true) {
+		let playerChoice = prompt(
+			"Please choose your hand: rock, paper, or scissors"
+		);
+
+		let loweredPlayerChoice = playerChoice.toLowerCase();
+
+		// console.log("Transformed %s -> %s", playerChoice, loweredPlayerChoice);
+
+		// checks if user input is valid
+		if (SELECTION.includes(loweredPlayerChoice)) {
+			console.log("%s is in SELECTION", playerChoice);
+			return loweredPlayerChoice;
+		} else {
+			alert("Please choose from the choices of rock, paper, or scissors");
+		}
+	}
+}
+
+function main() {
+	let playerSelection = computerPlay();
+	let computerSelection = computerPlay();
+
+	console.log("Player has %s", playerSelection);
+	console.log("Computer has %s", computerSelection);
+
+	console.log(
+		printOutcome(
+			playRound(playerSelection, computerSelection),
+			playerSelection,
+			computerSelection
+		)
+	);
+
+	game();
+}
+
+main();
